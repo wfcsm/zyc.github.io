43992bc2516b738ddc6849599a65df14b432cc41
zyc <627245048@qq.com>
2019-04-22 19:57:58 +0800

    会动简历

---
+++ js/resume.js
@@ -0,0 +1,269 @@
+let html = `<div class="main clearfix">
+<aside>
+    <div class="photo">
+        <img src="./cxk.jpg" alt="photo">
+        <h2>蔡徐坤</h2>
+        <h3>前端工程师</h3>
+    </div>
+    <div class="info">
+        <div class="about">
+            <h2>
+                <span class="tb">
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-wode"></use>
+                    </svg>
+                </span>
+                <span class="title">关于我</span>
+            </h2>
+            <p>
+                XXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxx
+                XXXXXXXXXXXXXXXXXXXXXXXXX
+                XXXXXXXXXXXXXXXXXXXXXXXX
+                XXXXXXXXXXXXXXXXXXXXXXXXX
+                XXXXXXXXXXXXXXXXXXXXXXXX
+                XXXXXXXXXXXXXXXXXXXXXXXXX
+            </p>
+        </div>
+        <div class="contact">
+            <h2>
+                <span class="tb">
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-contact"></use>
+                    </svg>
+                </span>
+                <span class="title">联系我</span>
+            </h2>
+            <ul >
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-dizhi"></use>
+                        </svg>
+                    </span>
+                    <span class="content">浙江省杭州市XXX街道XXX小区</span>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-weibiaoti103"></use>
+                        </svg>
+                    </span>
+                    <span class="content">zhang_yu_cheng@foxmail.com</span>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-phone"></use>
+                        </svg>
+                    </span>
+                    <span class="content">13429651911</span>
+                </li>
+                
+                
+            </ul>
+        </div>
+        <div class="skills">
+            <h2>
+                <span class="tb">
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-wangguan"></use>
+                    </svg>
+                </span>
+                <span class="title">掌握技能</span>
+            </h2>
+            <ul>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-socialjavascript"></use>
+                        </svg>
+                    </span>
+                    <div class="content">
+                        JavaScript
+                        <div class="bar" style="width: 90%"></div>
+                    </div>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-css"></use>
+                        </svg>
+                    </span>
+                    <div class="content">
+                        CSS & HTML
+                        <div class="bar" style="width: 90%"></div>
+                    </div>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-vuejs"></use>
+                        </svg>
+                    </span>
+                    <div class="content">
+                        Vue
+                        <div class="bar" style="width: 90%"></div>
+                    </div>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-react"></use>
+                        </svg>
+                    </span>
+                    <div class="content">
+                        React
+                        <div class="bar" style="width: 90%"></div>
+                    </div>
+                </li>
+                <li>
+                    <span class="tb-small">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-typescript"></use>
+                        </svg>
+                    </span>
+                    <div class="content">
+                        TypeScript
+                        <div class="bar" style="width: 90%"></div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</aside>
+<main>
+    <div class="project">
+        <h2>
+            <span class="tb">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-handbag"></use>
+                </svg>
+            </span>
+            <span class="title">项目经验</span>
+        </h2>
+        <div class="project-item">
+            <h3>项目一</h3>
+            <a href="">XXXXXXX@.com</a>
+            <article>
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+            </article>   
+        </div>
+
+        <div class="project-item">
+            <h3>项目二</h3>
+            <a href="">XXXXXXX@.com</a>
+            <article>
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+            </article>   
+        </div>
+
+        <div class="project-item">
+            <h3>项目三</h3>
+            <a href="">XXXXXXX@.com</a>
+            <article>
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+            </article>   
+        </div>
+
+        <div class="project-item">
+            <h3>项目四</h3>
+            <a href="">XXXXXXX@.com</a>
+            <article>
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+            </article>   
+        </div>
+
+        <div class="project-item">
+            <h3>项目三</h3>
+            <a href="">XXXXXXX@.com</a>
+            <article>
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+                项目一项目一项目一项目一项目一项目一项目项目一项目一
+            </article>   
+        </div>
+    </div>
+    <div class="project company">
+            <h2>
+                <span class="tb">
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-CompanyPages"></use>
+                    </svg>
+                </span>
+                <span class="title">工作经历</span>
+            </h2>
+            <div class="project-item">
+                <h3>XX公司</h3>
+                <article>
+                    是打发发生方法大沙发沙发上
+                    是打发发生方法大沙发沙发上
+                    是打发发生方法大沙发沙发上
+                </article>   
+            </div>
+            <div class="project-item">
+                <h3>XX公司</h3>
+                <article>
+                    是打发发生方法大沙发沙发上
+                    是打发发生方法大沙发沙发上
+                    是打发发生方法大沙发沙发上
+                </article>   
+            </div>
+            
+    </div>
+    <div class="project edu">
+        <h2>
+            <span class="tb">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-haofangtuoiconfont"></use>
+                </svg>
+            </span>
+            <span class="title">教育经历</span>
+        </h2>
+        <div class="project-item">
+            <h3>XXXX学校</h3>
+            <article>
+                是打发发生方法大沙发沙发上
+                是打发发生方法大沙发沙发上
+                是打发发生方法大沙发沙发上
+            </article>   
+        </div>    
+    </div>
+</main>
+</div>
+`
+
+var n =0;
+
+var id =setInterval(()=>{
+    n +=5;
+    codeHtml.innerHTML = html.substring(0,n);
+    main.innerHTML = codeHtml.innerHTML;
+    codeHtml.scrollTop= codeHtml.scrollHeight
+    if( n >= html.length && n>=css.length){
+        window.clearInterval(id)
+    }
+},1)
+
